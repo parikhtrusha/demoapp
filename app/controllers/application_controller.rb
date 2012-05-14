c83c5eedb182051dba509d3e12a9fb2a49402b5f
parikhtrusha <parikh.trushaparikh@gmail.com>
2012-05-14 17:04:52 -0400

    dsf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+ 
+ # before_filter :set_current_user  
+ # before_filter :login_required
+  protected
+  def set_current_user
+    if session[:user_id] 
+           @current_user = User.find(session[:user_id])
+       else
+           @current_user = nil
+       end
+  end
+
+  			def authenticate_user!
+  			if @current_user.nil?
+		    flash[:alert] = "You must be an admin to do that."
+    		redirect_to root_path
+  		end
+	end
+	
 end
