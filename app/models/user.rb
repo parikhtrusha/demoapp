314e99b25f8ab96fbdaf803be5a4b72eb6da06cb
parikhtrusha <parikh.trushaparikh@gmail.com>
2012-05-14 16:20:38 -0400

    fdgd

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+ 
+  # Setup accessible (or protected) attributes for your model
+ 
+end
